313f06a7878aa9fd0514b93f0b04df2d5a0091ec
Renato Oliveira Fraga <fraga@Renatos-iMac.local>
2021-03-12 19:07:03 -0600

    Initial commit.

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Renato Oliveira Fraga on 3/12/21.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
